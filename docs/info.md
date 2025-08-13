64985715aa259d041b581388edc8192ac7b9637e
SinghalNaina <zerotoasic@zerotoasic.vm.com>
2025-08-13 23:36:48 +0200

    commit-1:version-0

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,11 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
-
+The device will compare the threshold voltage and generate current
 ## How to test
 
-Explain how to use your project
+Apply input signals and measure the output current
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+Function generator and oscilloscope 
--- src/project.v
+++ src/project.v
@@ -5,10 +5,10 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_DalinEM_asic (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
-//    input  wire       VAPWR,    // 3.3v power supply
+    input  wire       VAPWR,    // 3.3v power supply
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -19,5 +19,41 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+
+     VCR subckt1(.vcc(VAPWR),.vss(VGND),.out(ua[0]),.vsen(ua[1]),.Vled(ua[2]));
+     latch subckt2(.VDD(VAPWR),.VSS(VGND),.S(ua[1]),.vsen(ua[1]));
+     COMP_2 subckt_3(.vcc(VAPWR),.vss(VGND));
+     BIAS_1 subckt_4(.vcc(VAPWR),.vss(VGND));
+     delay_1 subckt_5(.vcc(VAPWR),.vss(VGND));
+     Timming subckt_6(.vcc(VAPWR),.vss(VGND));
+     
+    
+    assign uo_out[0] = VGND;
+    assign uo_out[1] = VGND;
+    assign uo_out[2] = VGND;
+    assign uo_out[3] = VGND;
+    assign uo_out[4] = VGND;
+    assign uo_out[5] = VGND;
+    assign uo_out[6] = VGND;
+    assign uo_out[7] = VGND;
+
+    assign uio_out[0] = VGND;
+    assign uio_out[1] = VGND;
+    assign uio_out[2] = VGND;
+    assign uio_out[3] = VGND;
+    assign uio_out[4] = VGND;
+    assign uio_out[5] = VGND;
+    assign uio_out[6] = VGND;
+    assign uio_out[7] = VGND;
+
+    assign uio_oe[0] = VGND;
+    assign uio_oe[1] = VGND;
+    assign uio_oe[2] = VGND;
+    assign uio_oe[3] = VGND;
+    assign uio_oe[4] = VGND;
+    assign uio_oe[5] = VGND;
+    assign uio_oe[6] = VGND;
+    assign uio_oe[7] = VGND;
+
 
 endmodule
